7995eaa5f5547fac07ae5fda158ee04166f8ba16
daman85 <44984587+daman85@users.noreply.github.com>
2018-11-21 11:37:21 -0500

    Create PingAddress.ps1

---
+++ PingAddress.ps1
@@ -0,0 +1,78 @@
+﻿param(
+[Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+[string]$Address = "",
+[string]$Location = "",
+[string]$Backup = ""
+)
+
+
+
+$router = [Firewall]::new($Address)
+$router.Location = $Store
+$router.Backup = $Backup
+$router.ping()
+
+
+
+class Firewall {
+    [string]$IPaddress
+    [string]$Location
+    [string]$Backup
+    [int]$status = 1
+
+
+    Firewall([string]$ip){
+        $this.IPaddress = $ip
+    }
+
+    ping(){
+        $secpasswd = ConvertTo-SecureString "Dcz4848!$" -AsPlainText -Force
+        $mycreds = New-Object System.Management.Automation.PSCredential ("damian@48first.com", $secpasswd)
+        $from = "Damian  <damian@48first.com>"
+        $to = "Help <damian@48first.com>"
+        $smtp = "mail.48first.com"
+
+        
+        DO{
+        
+        $ok = Test-Connection $this.IPaddress -Count 5 -Quiet
+        if (-not($ok)){
+            
+            if ($this.status -eq(1) ){
+                
+                $this.status = 0
+                
+                $badSub = "Internet down at " + $this.Location  
+                $body = "PC IP: " + $this.IPaddress + "  Backup IP: "
+                
+                Send-MailMessage -To $to -From $from -Subject $badSub -Body $body.ToString()  -SmtpServer $smtp -Credential $mycreds
+
+            }
+        }
+        else { 
+            if ($this.status -eq(0)){
+                $this.status = 1
+                
+                $badSub = "Internet is back up at " + $this.Location 
+                $body = "PC IP: " + $this.IPaddress
+                
+
+                Send-MailMessage -To $to -From $from -Subject $badSub -Body $body.ToString()  -SmtpServer $smtp -Credential $mycreds
+            }
+        }
+        Start-Sleep 30
+        }while ($this.IPaddress)
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
